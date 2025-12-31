cc53b36305a659aed8334e50f7eb4ea776ff6564
hudchant <hudchant@gmail.com>
2025-12-30 17:31:29 -0800

    Add UI to console game

--- script.js
+++ script.js
@@ -1,133 +1,123 @@
 
-// Variable to track user's score
-let humanScore = 0;
+// User score
+let playerScore = 0;
 
-// Variable to track computer's score
+// Computer score
 let computerScore = 0;
 
+// Set to false by default
+let gameOver = false;
 
-// Create function to get the computer's choice
+// Get computer's choice
 function getComputerChoice() {
 
-    // Declare variable to eventually hold the value chosen by the computer
+    // Exit function when the game is over
+    if (gameOver) return;
+
     let computerPick;
 
-    // Get a random number between 0 and 3. Result will be either 0, 1, or 2.
+    // Get a random whole number - (either 0, 1, or 2)
     let randomNum = Math.floor(Math.random() * 3);
 
-    // Return a string response of rock, paper, or scissors deprending on the populated random number
     if (randomNum === 0) {
-        computerPick = 'rock'; // If random number is 0, the computerPick will be 'Rock'
+        computerPick = 'rock'; // 0 returns 'rock'
     } else if (randomNum === 1) {
-        computerPick = 'paper'; // If random number is 1, the computerPick will be 'Paper'
+        computerPick = 'paper'; // 1 returns 'paper'
     } else {
-        computerPick = 'scissors'; // If random number is 2, the computerPick will be 'Scissors'
+        computerPick = 'scissors'; // 2 returns 'scissors'
     }
-
-    // Return the computer's choice when the function is called
     return computerPick;
 }
 
-// Create function to get the user's choice
-function getHumanChoice() {
+// Initiate a round of gameplay
+function playRound(choice) {
 
-    // Initialize variable to hold the user's choice
-    let humanPick;
+    // Exit function when the game is over
+    if (gameOver) return;
 
-    // Prompt the user to enter their choice
-    let humanPickInitial = prompt('Please enter rock, paper, or scissors.');
+    const computerChoice = getComputerChoice(); // Get the computer's choice
 
-    // Make the user's prompt entry case insensitive
-    humanPick = humanPickInitial.toLowerCase();
+    const container = document.querySelector('.container'); // Reference to container div
 
-    // Return the user's choice when the function is called
-    return humanPick;
-}
+    const result = document.querySelector('.result'); // Reference to result div
 
-// Create function to initiate playing a round of the game
-function playRound(humanChoice, computerChoice) {
+    let runningScore = document.querySelector('.runningScore');
+    if (!runningScore) {
+        runningScore = document.createElement('div'); // Create score div if it doesn't already exist
+        runningScore.classList.add('runningScore'); // Give score div a class name for reference
+        container.appendChild(runningScore); // Append running score to parent div
+    }
 
-    // Use a switch to handle all user & computer cases and outcomes
-    switch (humanChoice) {
-        // If user and computer both choose 'rock', a tie is declared and no points are given
+    const finalScore = document.createElement('div'); // Display final score when game ends
+
+    const gameError = document.createElement('div'); // Display error, if one occurs during gameplay
+
+
+    // Determine round winner and distribute points accordingly
+    switch (choice) {
         case 'rock':
             if (computerChoice === 'rock') {
-                console.log('Tie - no points awarded.');
-                break;
-                // If user chooses 'rock' and computer chooses 'paper', computer wins and gets a point
+                // It's a tie. Score remains the same. 
             } else if (computerChoice === 'paper') {
-                console.log('You lose - paper beats rock.');
-                computerScore += 1;
-                break;
-                // If user chooses 'rock' and computer chooses 'scissors', user wins and gets a point
+                computerScore += 1; // Computer wins, increment score by 1
             } else if (computerChoice === 'scissors') {
-                console.log('You win - rock beats scissors.');
-                humanScore += 1;
-                break;
+                playerScore += 1; // Player wins, increment score by 1
             }
+            break;
         case 'paper':
-            // If user and computer both choose 'paper', a tie is declared and no points are given
             if (computerChoice === 'paper') {
-                console.log('Tie - no points awarded.');
-                break;
-                // If user chooses 'paper' and computer chooses 'rock', user wins and gets a point
+                // It's a tie. Score remains the same. 
             } else if (computerChoice === 'rock') {
-                console.log('You win - paper beats rock.');
-                humanScore += 1;
-                break;
-                // If user chooses 'paper' and computer chooses 'scissors', computer wins and gets a point
+                playerScore += 1; // Player wins, increment score by 1
             } else if (computerChoice === 'scissors') {
-                console.log('You lose - scissors beats paper.');
-                computerScore += 1;
-                break;
+                computerScore += 1; // Computer wins, increment score by 1
             }
+            break;
         case 'scissors':
-            // If user and computer both choose 'scissors', a tie is declared and no points are given
             if (computerChoice === 'scissors') {
-                console.log('Tie - no points awarded.');
-                break;
-                // If user chooses 'scissors' and computer chooses 'rock', computer wins and gets a point
+                // It's a tie. Score remains the same. 
             } else if (computerChoice === 'rock') {
-                console.log('You lose - rock beats scissors.');
-                computerScore += 1;
-                break;
-                // If user chooses 'scissors' and computer chooses 'paper', user wins and gets a point
+                computerScore += 1; // Computer wins, increment score by 1
             } else if (computerChoice === 'paper') {
-                console.log('You win - scissors beats paper.');
-                humanScore += 1;
-                break;
+                playerScore += 1; // Player wins, increment score by 1
             }
+            break;
         default:
-            console.log('Game error. Please try again.');
+            gameError.textContent = 'Game error. Please try again.';
+            container.appendChild(gameError); // Display error message
+            return; // Exit the game
+    }
+
+    // Running score message
+    runningScore.textContent = `You: ${playerScore} | Computer: ${computerScore}`;
+
+    // Round results message
+    result.textContent = `You selected "${choice}." The computer selected "${computerChoice}."`;
+
+    if (playerScore === 5) {
+        finalScore.textContent = 'Congratulations, you win the game!';
+        container.appendChild(finalScore); // Display final score message when player reaches 5 points first
+        gameOver = true; // Set gameOver variable to 'true' to exit function and end game
+
+    } else if (computerScore === 5) {
+        finalScore.textContent = 'Computer wins the game.';
+        container.appendChild(finalScore); // Display final score message when computer reaches 5 points first
+        gameOver = true; // Set gameOver variable to 'true' to exit function and end game
     }
 }
 
-// Create function to initiate playing five rounds of the game and declare a winner at the end
-function playGame() {
-
-    // Loop until all five rounds are played
-    for (let i = 1; i <= 5; i++) {
-
-        // Call the getHumanChoice function in all five rounds to get the user's choices
-        const humanSelection = getHumanChoice();
-        // Call the getComputerChoice function in all five rounds to get the computer's choices
-        const computerSelection = getComputerChoice();
-        // Call the playRound function to initiate one whole round of gameplay
-        playRound(humanSelection, computerSelection);
-
-        // Log user's choice, computer's choice, and both scores to the console at the end of each round
-        console.log('Your Choice:', `"${humanSelection}"`, 'Computer Choice:', `"${computerSelection}"`,
-            'Your Score:', `"${humanScore}"`, 'Computer Score:', `"${computerScore}"`
-        );
-
-    }
-
-    // Declare a winner at the end of all five rounds
-    humanScore > computerScore ? console.log('You win the game!') : console.log('Computer wins the game.');
-}
-
-// Call the playGame function to initiate gameplay
-playGame();
+// Listen for button clicks
+document.addEventListener('DOMContentLoaded', function () {
+    const allButtons = document.querySelectorAll('button');
+    allButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            // Set choice variable to button's string id to record user's choice
+            const choice = button.id;
+            // Call playRound function to start game upon user click 
+            playRound(choice);
+        });
+    });
+});
 
 
 
